f32ba9a610b38d0f1502a41d93c4e42a87db0d7d
alex <apachecordovax@gmail.com>
2021-07-28 19:03:56 +0200

    design de la home page

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,45 +1,47 @@
 <template>
   <q-layout view="lHh Lpr lFf">
-    <q-header elevated>
-      <q-toolbar>
+    <q-header class="bg-white text-grey-10" bordered>
+      <q-toolbar class="contain">
         <q-btn
+          to="/camera"
           flat
-          dense
           round
-          icon="menu"
-          aria-label="Menu"
-          @click="toggleLeftDrawer"
+          icon="eva-camera-outline"
+          size="15px"
+          class="large-screen-only q-mr-sm"
+          dense
         />
-
-        <q-toolbar-title>
-          Quasar App
+        <q-separator class="large-screen-only" vertical spaced />
+        <q-toolbar-title class="text-grand-hotel text-bold">
+          Quasagram
         </q-toolbar-title>
-
-        <div>Quasar v{{ $q.version }}</div>
+        <q-btn
+          to="/"
+          flat
+          round
+          icon="eva-home-outline"
+          size="15px"
+          class="large-screen-only"
+          dense
+        />
       </q-toolbar>
     </q-header>
 
-    <q-drawer
-      v-model="leftDrawerOpen"
-      show-if-above
-      bordered
-      class="bg-grey-1"
-    >
-      <q-list>
-        <q-item-label
-          header
-          class="text-grey-8"
-        >
-          Essential Links
-        </q-item-label>
-
-        <EssentialLink
-          v-for="link in essentialLinks"
-          :key="link.title"
-          v-bind="link"
+    <q-footer class="bg-white small-screen-only" bordered>
+      <q-tabs
+        class="text-grey-10"
+        active-color="primary"
+        indicator-color="transparent"
+      >
+        <q-route-tab to="/" name="mail" icon="eva-home-outline" label="Home" />
+        <q-route-tab
+          to="/camera"
+          name="camera"
+          icon="eva-camera-outline"
+          label="Camera"
         />
-      </q-list>
-    </q-drawer>
+      </q-tabs>
+    </q-footer>
 
     <q-page-container>
       <router-view />
@@ -48,72 +50,22 @@
 </template>
 
 <script>
-import EssentialLink from 'components/EssentialLink.vue'
-
-const linksList = [
-  {
-    title: 'Docs',
-    caption: 'quasar.dev',
-    icon: 'school',
-    link: 'https://quasar.dev'
-  },
-  {
-    title: 'Github',
-    caption: 'github.com/quasarframework',
-    icon: 'code',
-    link: 'https://github.com/quasarframework'
-  },
-  {
-    title: 'Discord Chat Channel',
-    caption: 'chat.quasar.dev',
-    icon: 'chat',
-    link: 'https://chat.quasar.dev'
-  },
-  {
-    title: 'Forum',
-    caption: 'forum.quasar.dev',
-    icon: 'record_voice_over',
-    link: 'https://forum.quasar.dev'
-  },
-  {
-    title: 'Twitter',
-    caption: '@quasarframework',
-    icon: 'rss_feed',
-    link: 'https://twitter.quasar.dev'
-  },
-  {
-    title: 'Facebook',
-    caption: '@QuasarFramework',
-    icon: 'public',
-    link: 'https://facebook.quasar.dev'
-  },
-  {
-    title: 'Quasar Awesome',
-    caption: 'Community Quasar projects',
-    icon: 'favorite',
-    link: 'https://awesome.quasar.dev'
-  }
-];
-
-import { defineComponent, ref } from 'vue'
+import { defineComponent, ref } from "vue";
 
 export default defineComponent({
-  name: 'MainLayout',
-
-  components: {
-    EssentialLink
-  },
-
-  setup () {
-    const leftDrawerOpen = ref(false)
-
-    return {
-      essentialLinks: linksList,
-      leftDrawerOpen,
-      toggleLeftDrawer () {
-        leftDrawerOpen.value = !leftDrawerOpen.value
-      }
-    }
-  }
-})
+  name: "MainLayout",
+});
 </script>
+
+<style lang="sass">
+.q-toolbar
+  @media ( min-width:  $breakpoint-sm-min)
+    height: 77px
+.q-toolbar__title
+  font-size: 30px
+  @media ( max-width:  $breakpoint-xs-max)
+    text-align: center
+.q-footer
+  .q-tab__icon
+    font-size: 30px
+</style>
---
+++ src/pages/PageHome.vue
@@ -0,0 +1,110 @@
+<template>
+  <q-page class="contain q-pa-md">
+    <div class="row q-col-gutter-lg">
+      <div class="col-12 col-sm-8">
+        <q-card
+          v-for="post in posts"
+          :key="post.id"
+          class="card-post q-mb-md"
+          flat
+          bordered
+        >
+          <q-item>
+            <q-item-section avatar>
+              <q-avatar>
+                <img
+                  src="https://secure.gravatar.com/avatar/3a7e9702158dcb9a35aa8edd451913fa?s=64"
+                />
+              </q-avatar>
+            </q-item-section>
+
+            <q-item-section>
+              <q-item-label class="text-bold">alex__esaki</q-item-label>
+              <q-item-label caption>{{ post.location }}</q-item-label>
+            </q-item-section>
+          </q-item>
+
+          <q-separator />
+
+          <q-img :src="post.imageUrl" />
+
+          <q-card-section>
+            <div>{{ post.caption }}</div>
+            <div class="text-caption text-grey">{{ niceDate(post.date) }}</div>
+          </q-card-section>
+        </q-card>
+      </div>
+      <div class="col-4">
+        <q-item class="fixed large-screen-only">
+          <q-item-section avatar>
+            <q-avatar>
+              <img
+                src="https://secure.gravatar.com/avatar/3a7e9702158dcb9a35aa8edd451913fa?s=64"
+              />
+            </q-avatar>
+          </q-item-section>
+
+          <q-item-section>
+            <q-item-label class="text-bold">alex__esaki</q-item-label>
+            <q-item-label></q-item-label>
+            <q-item-label caption>Alex Esaki</q-item-label>
+          </q-item-section>
+        </q-item>
+      </div>
+    </div>
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+import { date } from "quasar";
+
+export default defineComponent({
+  name: "PageIndex",
+  data() {
+    return {
+      posts: [
+        {
+          id: 1,
+          caption: "Golden Date Bridge",
+          date: 1627488889578,
+          location: "San Francisco, United States",
+          imageUrl: "https://cdn.quasar.dev/img/parallax2.jpg",
+        },
+        {
+          id: 2,
+          caption: "Golden Date Bridge",
+          date: 1627488889578,
+          location: "San Francisco, United States",
+          imageUrl: "https://cdn.quasar.dev/img/parallax2.jpg",
+        },
+        {
+          id: 3,
+          caption: "Golden Date Bridge",
+          date: 1627488889578,
+          location: "San Francisco, United States",
+          imageUrl: "https://cdn.quasar.dev/img/parallax2.jpg",
+        },
+        {
+          id: 4,
+          caption: "Golden Date Bridge",
+          date: 1627488889578,
+          location: "San Francisco, United States",
+          imageUrl: "https://cdn.quasar.dev/img/parallax2.jpg",
+        },
+      ],
+    };
+  },
+  methods: {
+    niceDate(value) {
+      return date.formatDate(value, "MMMM D h:mmA");
+    },
+  },
+});
+</script>
+
+<style lang="sass">
+.card-post
+  .q-img
+    min-height: 200px
+</style>
